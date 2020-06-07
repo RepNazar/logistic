657744750f573a512b790bd6633b30241603a9c7
NazarRep <Repyanski@mail.ru>
2020-06-07 14:47:43 +0300

    N+1_QueryProblem_Fixed

--- src/main/java/ua/Nazar/Rep/logistic/repo/CarRepo.java
+++ src/main/java/ua/Nazar/Rep/logistic/repo/CarRepo.java
@@ -1,7 +1,13 @@
 package ua.Nazar.Rep.logistic.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.Nazar.Rep.logistic.domain.Car;
 
 public interface CarRepo extends JpaRepository<Car, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"model.mark"})
+    Page<Car> findAll(Pageable pageable);
 }
--- src/main/java/ua/Nazar/Rep/logistic/repo/ModelRepo.java
+++ src/main/java/ua/Nazar/Rep/logistic/repo/ModelRepo.java
@@ -1,7 +1,13 @@
 package ua.Nazar.Rep.logistic.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.Nazar.Rep.logistic.domain.Model;
 
 public interface ModelRepo extends JpaRepository<Model, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"mark"})
+    Page<Model> findAll(Pageable pageable);
 }
